bfb162c9072a840871ad6a24986c0ecd16126983
lafleur <lafleur@boum.org>
2025-11-22 12:26:29 +0100

    OidcIssuer: add debug logs

--- src/oidc/endpoint/issuer.rs
+++ src/oidc/endpoint/issuer.rs
@@ -112,25 +112,26 @@ where
 	T: DeviceStore,
 {
 	async fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
-		debug!("issuing token for {grant:?}");
+		debug!(?grant, "issuer creating token");
 		let user_id = UserId::parse_with_server_name(&grant.owner_id, &self.server_name)
 			.expect("valid owner id in grant");
-		trace!("deserialising device id");
+		trace!(?user_id, "got user_id, deserialising device id");
 		let device_id = deviceid_from_scope(grant.scope.clone())
 			.expect("valid device id from the grant's scope");
+		trace!(?device_id, "got device_id, search for client");
 		let client = self
 			.get_client(&grant.client_id)
 			.await
 			.map_err(|_| ())?
 			.ok_or(())?;
+		trace!(?client, "issuer got client");
+
 		let access_token = self.devices.generate_token().await;
 		let refresh_token = self.devices.generate_token().await;
 		let until = Utc::now() + Duration::milliseconds(self.token_ttl);
 		let refresh_until = Utc::now() + Duration::milliseconds(self.refresh_ttl);
-
-		trace!("checking if the device is registered");
 		// Register the device if not registered in the owner devices list.
-		if !self.devices.exists(&user_id, &device_id).await {
+		if ! self.devices.exists(&user_id, &device_id).await {
 			let oidc_device = OidcDevice {
 				client_id: grant.client_id,
 				scope: grant.scope,
@@ -138,41 +139,40 @@ where
 				// TODO deal with the possible overflow.
 				until: until.timestamp_millis() as u64,
 			};
-			trace!("saving device metadata");
+			trace!(?oidc_device, "issuer adding oidc device to device store");
 			self.devices
 				.create(&user_id, &device_id, &access_token, client.name, None)
 				.await
 				.map_err(|_| ())?;
-			trace!("saving device OIDC details");
 			let key = (&user_id, &device_id);
 			self.userdeviceid_oidcdevice.put(key, Json(oidc_device));
 		}
 
 		// Store the device's login token.
-		trace!("saving device's access token");
 		self.devices
 			.set_token(&user_id, &device_id, &access_token)
 			.await
 			.map_err(|_| ())?;
 
 		// Store the device's refresh token with the device id.
-		trace!("saving device's refresh token");
 		let value = ((user_id, device_id), refresh_until.timestamp_millis());
+		trace!(?refresh_token, ?value, "saving device refresh token's expiry date");
 		self.refreshtoken_userdeviceidexpiresat
 			.put(refresh_token.clone(), value);
 
-		trace!("returning token");
-		Ok(IssuedToken {
+		let token = IssuedToken {
 			token: access_token,
 			refresh: Some(refresh_token),
 			until,
 			token_type: TokenType::Bearer,
-		})
+		};
+		debug!(?token, "returning token");
+
+		Ok(token)
 	}
 
-	async fn refresh(&mut self, refresh: &str, grant: Grant) -> Result<RefreshedToken, ()> {
-		let expected_device_id = deviceid_from_scope(grant.scope.clone()).map_err(|_| ())?;
-		trace!("getting refresh token's expiration date");
+	async fn refresh(&mut self, refresh: &str, _grant: Grant) -> Result<RefreshedToken, ()> {
+		debug!("getting refresh token's expiration date");
 		let ((user_id, device_id), expires_at): ((OwnedUserId, OwnedDeviceId), i64) = self
 			.refreshtoken_userdeviceidexpiresat
 			.get(refresh)
@@ -206,24 +206,30 @@ where
 		self.refreshtoken_userdeviceidexpiresat
 			.put(new_refresh.clone(), value);
 
-		Ok(RefreshedToken {
+		let token = RefreshedToken {
 			token: new_access,
 			refresh: Some(new_refresh),
 			until,
 			token_type: TokenType::Bearer,
-		})
+		};
+		debug!(?token, "returning token");
+
+		Ok(token)
 	}
 
 	async fn recover_token<'a>(&'a mut self, token: &str) -> Result<Option<Grant>, ()> {
+		debug!(?token, "issuer recovering token");
 		let (user_id, device_id) = self.devices.find(token).await.map_err(|_| ())?;
+		trace!(?device_id, "issuer found device id, search device");
 		let device = self
 			.get_device(&user_id, &device_id)
 			.await
 			.map_err(|_| ())?;
+		trace!(?device, "issuer found device, check expiration");
 
 		// Check that the device is not expired.
 		if device.until < millis_since_unix_epoch() {
-			trace!(?user_id, ?device_id, ?token, "removing expired device");
+			trace!(?user_id, ?device_id, ?token, "issuer removing expired device");
 			self.devices.remove(&user_id, &device_id).await;
 			return Err(());
 		}
@@ -240,11 +246,13 @@ where
 			// TODO understand what extensions are.
 			extensions: Extensions::new(),
 		};
+		debug!(?grant, "issuer returning grant");
 
 		Ok(Some(grant))
 	}
 
 	async fn recover_refresh<'a>(&'a mut self, refresh: &str) -> Result<Option<Grant>, ()> {
+		debug!(?refresh, "issuer recovering refresh");
 		// First check that the token exists.
 		let ((user_id, device_id), expires_at): ((OwnedUserId, OwnedDeviceId), i64) = self
 			.refreshtoken_userdeviceidexpiresat
@@ -253,14 +261,16 @@ where
 			.map_err(|_| ())?
 			.deserialized()
 			.map_err(|_| ())?;
+		trace!(?device_id, ?expires_at, "issuer found device id, search device");
 		let device = self
 			.get_device(&user_id, &device_id)
 			.await
 			.map_err(|_| ())?;
+		trace!(?device, "issuer got device, check expiration");
 
 		// Then check that it's not expired.
 		if (expires_at as u64) < millis_since_unix_epoch() {
-			trace!(?device, ?refresh, "removing expired device refresh token");
+			trace!(?device, ?refresh, "issuer removing expired device refresh token");
 			self.devices.remove(&user_id, &device_id).await;
 			return Err(());
 		}
@@ -276,6 +286,7 @@ where
 			until,
 			extensions: Extensions::new(),
 		};
+		debug!(?grant, "issuer returning grant");
 
 		Ok(Some(grant.into()))
 	}
