3f02d1c0ff3ccd047dce22477499af7f1ff3ded6
lafleur <lafleur@boum.org>
2025-11-28 02:34:23 +0100

    move OidcEndpoint to its separate source file

--- src/oidc/endpoint/mod.rs
+++ src/oidc/endpoint/mod.rs
@@ -1,109 +1,21 @@
-use axum::async_trait;
-use oxide_auth::{
-	endpoint::{OAuthError, Scope, Scopes, Template, WebRequest},
-	frontends::simple::extensions::AddonList,
-	primitives::{
-		prelude::{AuthMap, RandomGenerator},
-		registrar::RegisteredUrl,
-	},
-};
-use oxide_auth_async::{
-	endpoint::{Endpoint, Extension, OwnerSolicitor},
-	primitives::{Authorizer, Issuer, Registrar},
-};
+use oxide_auth::primitives::registrar::RegisteredUrl;
 use url::Url;
 
 mod device_store;
-pub use device_store::DeviceStore;
-mod registrar;
-pub use registrar::{OidcClient, OidcRegistrar};
-mod solicitor;
-pub use solicitor::AsyncSolicitor;
+mod endpoint;
 mod issuer;
-pub use issuer::{OidcDevice, OidcIssuer};
-
-use crate::{OidcError, OidcRequest, OidcResponse};
+mod registrar;
+mod solicitor;
+pub use self::{
+	device_store::DeviceStore,
+	endpoint::OidcEndpoint,
+	issuer::{OidcDevice, OidcIssuer},
+	registrar::{OidcClient, OidcRegistrar},
+	solicitor::AsyncSolicitor,
+};
 
 pub const SCOPE_PREFIX_DEVICE: &str = "urn:matrix:org.matrix.msc2967.client:device:";
 pub const SCOPE_PREFIX_API: &str = "urn:matrix:org.matrix.msc2967.client:api:";
-
-pub struct OidcEndpoint<R, I>
-where
-	R: Registrar,
-	I: Issuer,
-{
-	pub authorizer: AuthMap<RandomGenerator>,
-	pub registrar: R,
-	pub issuer: I,
-	pub solicitor: AsyncSolicitor,
-	pub extension: AddonList,
-	pub scopes: Vec<Scope>,
-}
-
-impl<R, I> OidcEndpoint<R, I>
-where
-	R: Registrar + Sync + Send,
-	I: Issuer + Sync + Send,
-{
-	pub fn from_primitives(registrar: R, issuer: I, solicitor: AsyncSolicitor) -> Self {
-		let authorizer = AuthMap::new(RandomGenerator::new(16));
-		let extension = AddonList::new();
-		let scopes = Vec::new();
-
-		OidcEndpoint {
-			authorizer,
-			registrar,
-			issuer,
-			solicitor,
-			extension,
-			scopes,
-		}
-	}
-}
-
-#[async_trait]
-impl<R, I> Endpoint<OidcRequest> for &mut OidcEndpoint<R, I>
-where
-	R: Registrar + Sync + Send,
-	I: Issuer + Sync + Send,
-{
-	type Error = OidcError;
-
-	fn registrar(&self) -> Option<&(dyn Registrar + Sync)> { Some(&self.registrar) }
-
-	fn authorizer_mut(&mut self) -> Option<&mut (dyn Authorizer + Send)> {
-		Some(&mut self.authorizer)
-	}
-
-	fn issuer_mut(&mut self) -> Option<&mut (dyn Issuer + Send)> { Some(&mut self.issuer) }
-
-	fn owner_solicitor(&mut self) -> Option<&mut (dyn OwnerSolicitor<OidcRequest> + Send)> {
-		Some(&mut self.solicitor)
-	}
-
-	fn scopes(&mut self) -> Option<&mut dyn Scopes<OidcRequest>> { Some(&mut self.scopes) }
-
-	fn response(
-		&mut self,
-		_request: &mut OidcRequest,
-		_kind: Template<'_>,
-	) -> Result<<OidcRequest as WebRequest>::Response, Self::Error> {
-		// TODO check.
-		Ok(OidcResponse::default())
-	}
-
-	fn error(&mut self, err: OAuthError) -> Self::Error {
-		match err {
-			| OAuthError::DenySilently => OidcError::Authorization,
-			| OAuthError::BadRequest => OidcError::Encoding,
-			| OAuthError::PrimitiveError => OidcError::InternalError(None),
-		}
-	}
-
-	fn web_error(&mut self, err: OidcError) -> Self::Error { err }
-
-	fn extension(&mut self) -> Option<&mut (dyn Extension + Send)> { None }
-}
 
 /// Substitute "127.0.0.1" and "[::1]" for "localhost" to let oxide-auth compare
 /// them ignoring their port.
