4b4586a238d25d6206e23f04740d317177828919
Shane Jaroch <chown_tee@proton.me>
2026-02-25 06:46:55 -0500

    allow DELETE, fix 400 error (want 401), see below:
    This error occurred fairly early in the Complement logs:
    
    ```log
    === NAME  TestDeviceManagement/DELETE_/device/{deviceId}
        apidoc_device_management_test.go:134: MatchResponse got status 400 want 401 - http://127.0.0.1:32820/_matrix/client/v3/devices/7hTYOfr7hy => {"errcode":"M_NOT_JSON","error":"M_NOT_JSON: Not json."}
    === RUN   TestDeviceManagement/DELETE_/device/{deviceId}_requires_UI_auth_user_to_match_device_owner
    ```

--- src/api/router/args.rs
+++ src/api/router/args.rs
@@ -96,7 +96,9 @@ where
 		// appservices still call APIs like /join like this. so let's just default to
 		// empty object `{}` to copy synapse's behaviour
 		if json_body.is_none()
-			&& request.parts.method == http::Method::POST
+			&& (request.parts.method == http::Method::POST
+				|| request.parts.method == http::Method::PUT
+				|| request.parts.method == http::Method::DELETE)
 			&& !request.parts.uri.path().contains("/media/")
 		{
 			trace!("json_body from_request: {:?}", json_body.clone());
