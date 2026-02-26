3c47b97de07e4571d5603bb9f29c04af7ecf9b8f
Shane Jaroch <chown_tee@proton.me>
2026-02-25 23:28:57 -0500

    feat(api/timestamp_to_event): properly implement API endpoint according to msc3030 utilizing binary search

---
+++ src/api/client/room/timestamp.rs
@@ -0,0 +1,102 @@
+use axum::extract::State;
+use conduwuit::{Err, PduCount, Result};
+use futures::{StreamExt, pin_mut};
+use ruma::{
+	MilliSecondsSinceUnixEpoch,
+	api::{Direction, client::room::get_event_by_timestamp},
+};
+
+use crate::Ruma;
+
+/// # `GET /_matrix/client/unstable/org.matrix.msc3030/rooms/{roomId}/timestamp_to_event`
+///
+/// Get the ID of the event closest to the given timestamp.
+pub(crate) async fn get_room_event_by_timestamp_route(
+	State(services): State<crate::State>,
+	body: Ruma<get_event_by_timestamp::v1::Request>,
+) -> Result<get_event_by_timestamp::v1::Response> {
+	let room_id = &body.room_id;
+	let ts = body.ts;
+	let dir = body.dir;
+
+	if !services
+		.rooms
+		.state_cache
+		.is_joined(body.sender_user(), room_id)
+		.await
+	{
+		return Err!(Request(Forbidden(
+			"You don't have permission to view events in this room."
+		)));
+	}
+
+	let target_ts = ts;
+	let mut event = None;
+
+	let mut low = 0_i64;
+	let mut high = services
+		.rooms
+		.timeline
+		.last_timeline_count(room_id)
+		.await
+		.unwrap_or(PduCount::max())
+		.into_signed();
+
+	while low <= high {
+		let mid = low.saturating_add(high.saturating_sub(low) / 2);
+		let pdus = services
+			.rooms
+			.timeline
+			.pdus(room_id, Some(PduCount::from_signed(mid)));
+		pin_mut!(pdus);
+
+		let mut found_pdu = None;
+		let mut found_count = 0_i64;
+
+		while let Some(Ok((count, pdu))) = pdus.next().await {
+			if services
+				.rooms
+				.state_accessor
+				.user_can_see_event(body.sender_user(), room_id, &pdu.event_id)
+				.await
+			{
+				found_pdu = Some(pdu);
+				found_count = count.into_signed();
+				break;
+			}
+		}
+
+		if let Some(pdu) = found_pdu {
+			let pdu_ts = MilliSecondsSinceUnixEpoch(pdu.origin_server_ts);
+
+			if dir == Direction::Forward {
+				if pdu_ts >= target_ts {
+					event = Some(pdu);
+					high = mid.saturating_sub(1);
+				} else {
+					low = found_count.saturating_add(1);
+				}
+			} else {
+				// dir == Direction::Backward
+				if pdu_ts <= target_ts {
+					event = Some(pdu);
+					low = found_count.saturating_add(1);
+				} else {
+					high = mid.saturating_sub(1);
+				}
+			}
+		} else {
+			// No visible events from mid onwards. Search before mid.
+			high = mid.saturating_sub(1);
+		}
+	}
+
+	if let Some(event) = event {
+		Ok(get_event_by_timestamp::v1::Response::new(
+			event.event_id.clone(),
+			MilliSecondsSinceUnixEpoch(event.origin_server_ts),
+		))
+	} else {
+		Err!(Request(NotFound("No event found for the given timestamp")))
+	}
+}
