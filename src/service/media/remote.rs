5f84b74f3834de5b8602a81152d5738a3b19e24c
Shane Jaroch <chown_tee@proton.me>
2026-03-01 02:38:54 -0500

    fix merge

--- src/service/media/remote.rs
+++ src/service/media/remote.rs
@@ -35,15 +35,10 @@ pub async fn fetch_remote_thumbnail(
 		.fetch_thumbnail_authenticated(mxc, user, server, timeout_ms, dim)
 		.await;
 
-	if let Err(error) = &result {
-		if matches!(
-			error.kind(),
-			NotFound | Unrecognized | Unauthorized | Forbidden { .. } | Unknown
-		) {
-			return self
-				.fetch_thumbnail_unauthenticated(mxc, user, server, timeout_ms, dim)
-				.await;
-		}
+	if let Err(Error::Request(NotFound, ..)) = &result {
+		return self
+			.fetch_thumbnail_unauthenticated(mxc, user, server, timeout_ms, dim)
+			.await;
 	}
 
 	result
@@ -72,15 +67,10 @@ pub async fn fetch_remote_content(
 			);
 		});
 
-	if let Err(error) = &result {
-		if matches!(
-			error.kind(),
-			NotFound | Unrecognized | Unauthorized | Forbidden { .. } | Unknown
-		) {
-			return self
-				.fetch_content_unauthenticated(mxc, user, server, timeout_ms)
-				.await;
-		}
+	if let Err(Error::Request(Unrecognized, ..)) = &result {
+		return self
+			.fetch_content_unauthenticated(mxc, user, server, timeout_ms)
+			.await;
 	}
 
 	result
