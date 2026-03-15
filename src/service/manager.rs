180b08c0f0d3d18bad998d9ff78b2e9535716af9
Shane Jaroch <chown_tee@proton.me>
2026-03-15 17:23:22 -0400

    better?

--- src/service/manager.rs
+++ src/service/manager.rs
@@ -34,11 +34,10 @@ impl Manager {
 	}
 
 	pub(super) async fn poll(&self) -> Result<()> {
-		let mut lock = self.manager.lock().await;
-		if let Some(manager) = &mut *lock {
+		let manager_handle = self.manager.lock().await.take();
+		if let Some(manager) = manager_handle {
 			trace!("Polling service manager...");
 			let res = manager.await;
-			*lock = None;
 			return res.map_err(Error::from).and_then(|res| res);
 		}
 
