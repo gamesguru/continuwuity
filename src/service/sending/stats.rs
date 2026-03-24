255452ec738465c7600df38258ea0bef3eb274e9
Shane Jaroch <chown_tee@proton.me>
2026-03-24 05:19:51 -0400

    granularize federation stats logging by sub-types

--- src/service/sending/stats.rs
+++ src/service/sending/stats.rs
@@ -8,7 +8,11 @@ use conduwuit::info;
 pub struct FederationStats {
 	pub outgoing_txns: AtomicU64,
 	pub outgoing_pdus: AtomicU64,
-	pub outgoing_edus: AtomicU64,
+	pub outgoing_edus: AtomicU64, // Catch-all for unknown
+	pub outgoing_receipts: AtomicU64,
+	pub outgoing_device_lists: AtomicU64,
+	pub outgoing_to_device: AtomicU64,
+	pub outgoing_typing: AtomicU64,
 	pub outgoing_presence: AtomicU64,
 	pub outgoing_errors: AtomicU64,
 }
@@ -20,6 +24,10 @@ impl FederationStats {
 		let txns = self.outgoing_txns.swap(0, Ordering::Relaxed);
 		let pdus = self.outgoing_pdus.swap(0, Ordering::Relaxed);
 		let edus = self.outgoing_edus.swap(0, Ordering::Relaxed);
+		let receipts = self.outgoing_receipts.swap(0, Ordering::Relaxed);
+		let device_lists = self.outgoing_device_lists.swap(0, Ordering::Relaxed);
+		let to_device = self.outgoing_to_device.swap(0, Ordering::Relaxed);
+		let typing = self.outgoing_typing.swap(0, Ordering::Relaxed);
 		let presence = self.outgoing_presence.swap(0, Ordering::Relaxed);
 		let errors = self.outgoing_errors.swap(0, Ordering::Relaxed);
 
@@ -28,8 +36,9 @@ impl FederationStats {
 		}
 
 		info!(
-			"federation stats: {txns} txns ({pdus} PDUs, {edus} EDUs, {presence} presence), \
-			 {errors} errors"
+			"federation stats: {txns} txns ({pdus} PDUs, {edus} general EDUs, \
+			 {presence} presence, {receipts} receipts, {device_lists} device lists, \
+			 {to_device} to-device, {typing} typing), {errors} errors"
 		);
 
 		true
