c03b51d7b34d655d66c7fba21bc2f7fbbca2658f
Shane Jaroch <chown_tee@proton.me>
2026-03-26 05:45:49 -0400

    perf(presence): cache user_sees_user checks
    Adds a 5-minute `user_visibility_cache` to `state_cache` to eliminate heavy RocksDB iterations during presence updates. This prevents active client sync loops from generating massive disk reads when processing live presence streams.

--- src/service/rooms/state_cache/mod.rs
+++ src/service/rooms/state_cache/mod.rs
@@ -23,6 +23,7 @@ use crate::{Dep, account_data, appservice::RegistrationInfo, config, globals, ro
 pub struct Service {
 	appservice_in_room_cache: AppServiceInRoomCache,
 	server_visibility_cache: Cache<(OwnedServerName, OwnedUserId), bool>,
+	user_visibility_cache: Cache<(OwnedUserId, OwnedUserId), bool>,
 	services: Services,
 	db: Data,
 }
@@ -64,6 +65,20 @@ impl crate::Service for Service {
 			appservice_in_room_cache: SyncRwLock::new(HashMap::new()),
 			// Ugly way to build the cache with a dynamic capacity
 			server_visibility_cache: Cache::builder()
+				.max_capacity(
+					conduwuit::utils::math::usize_from_f64(
+						25_000.0
+							* args
+								.depend::<config::Service>("config")
+								.cache_capacity_modifier,
+					)
+					.unwrap_or(25_000)
+					.try_into()
+					.unwrap_or(25_000),
+				)
+				.time_to_idle(std::time::Duration::from_secs(5 * 60))
+				.build(),
+			user_visibility_cache: Cache::builder()
 				.max_capacity(
 					conduwuit::utils::math::usize_from_f64(
 						25_000.0
@@ -218,10 +233,23 @@ pub async fn server_sees_user(&self, server: &ServerName, user_id: &UserId) -> b
 #[implement(Service)]
 #[tracing::instrument(skip(self), level = "trace")]
 pub async fn user_sees_user(&self, user_a: &UserId, user_b: &UserId) -> bool {
+	let key = if user_a < user_b {
+		(user_a.to_owned(), user_b.to_owned())
+	} else {
+		(user_b.to_owned(), user_a.to_owned())
+	};
+
+	if let Some(sees) = self.user_visibility_cache.get(&key) {
+		return sees;
+	}
+
 	let get_shared_rooms = self.get_shared_rooms(user_a, user_b);
 
 	pin_mut!(get_shared_rooms);
-	get_shared_rooms.next().await.is_some()
+	let sees = get_shared_rooms.next().await.is_some();
+
+	self.user_visibility_cache.insert(key, sees);
+	sees
 }
 
 /// List the rooms common between two users
