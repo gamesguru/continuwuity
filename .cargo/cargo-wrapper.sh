4699917cc436aa1212aff054552f2617b3afd7a0
Shane Jaroch <chown_tee@proton.me>
2026-04-18 21:56:41 -0400

    Revert "build: make clang and mold linkers optional with warnings"
    This reverts commit 09308f946fdaef59c53dd06609720df07ad93b61.

--- .cargo/cargo-wrapper.sh
+++ .cargo/cargo-wrapper.sh
@@ -10,30 +10,14 @@ if [[ -z "$NO_SCCACHE" ]] && command -v sccache >/dev/null 2>&1; then
     CMD=(sccache "$RUSTC")
 fi
 
-EXTRA_ARGS=()
-
-# Clang detection
-if command -v clang >/dev/null 2>&1; then
-    EXTRA_ARGS+=("-C" "linker=clang")
-else
-    # Only warn once (check if we're compiling a top-level crate or just some build script)
-    if [[ "$*" == *" --crate-name conduwuit "* ]]; then
-        echo "warning: clang not found, falling back to default linker" >&2
-    fi
-fi
-
-# Mold detection
+MOLD_ARGS=()
 if command -v mold >/dev/null 2>&1; then
     # Do not use mold if cross-compiling to webassembly or riscv (SP1)
     if [[ "$*" == *"wasm32"* ]] || [[ "$*" == *"riscv"* ]]; then
         : # skip mold
     else
-        EXTRA_ARGS+=("-C" "link-arg=-fuse-ld=mold")
-    fi
-else
-    if [[ "$*" == *" --crate-name conduwuit "* ]]; then
-        echo "warning: mold not found, falling back to default linker" >&2
+        MOLD_ARGS=("-C" "link-arg=-fuse-ld=mold")
     fi
 fi
 
-exec "${CMD[@]}" "$@" "${EXTRA_ARGS[@]}"
+exec "${CMD[@]}" "$@" "${MOLD_ARGS[@]}"
