64246440076e6df07a2a55d44432596eba66cfee
Shane Jaroch <chown_tee@proton.me>
2026-04-18 23:44:28 -0400

    more refactor/linting

--- src/service/rooms/state_accessor/server_can.rs
+++ src/service/rooms/state_accessor/server_can.rs
@@ -1,5 +1,5 @@
 use conduwuit::implement;
-use futures::{StreamExt, future::ready};
+use futures::StreamExt;
 use ruma::{
 	EventId, RoomId, ServerName,
 	events::{
@@ -50,18 +50,19 @@ pub async fn server_can_see_event(
 		| HistoryVisibility::WorldReadable => true,
 		| HistoryVisibility::Shared | HistoryVisibility::Invited => {
 			// Allow if any member on requesting server is AT LEAST invited, else deny
-			let members = self
+			let members: Vec<ruma::OwnedUserId> = self
 				.services
 				.state_cache
-				.room_members(room_id)
-				.chain(self.services.state_cache.room_members_invited(room_id))
+				.room_members_invited(room_id)
+				.chain(self.services.state_cache.room_members(room_id))
 				.map(ToOwned::to_owned)
-				.filter(|member| ready(member.server_name() == origin))
-				.collect::<Vec<_>>()
+				.collect()
 				.await;
 
 			for member in members {
-				if self.user_was_invited(shortstatehash, &member).await {
+				if member.server_name() == origin
+					&& self.user_was_invited(shortstatehash, &member).await
+				{
 					return true;
 				}
 			}
@@ -70,23 +71,24 @@ pub async fn server_can_see_event(
 		},
 		| HistoryVisibility::Joined => {
 			// Allow if any member on requesting server is joined, else deny
-			let members = self
+			let members: Vec<ruma::OwnedUserId> = self
 				.services
 				.state_cache
 				.room_members(room_id)
 				.map(ToOwned::to_owned)
-				.filter(|member| ready(member.server_name() == origin))
-				.collect::<Vec<_>>()
+				.collect()
 				.await;
 
 			for member in members {
-				if self.user_was_joined(shortstatehash, &member).await {
+				if member.server_name() == origin
+					&& self.user_was_joined(shortstatehash, &member).await
+				{
 					return true;
 				}
 			}
 
 			false
 		},
-		| _ => true,
+		| _ => false,
 	}
 }
