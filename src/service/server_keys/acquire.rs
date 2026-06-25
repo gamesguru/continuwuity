09ecc9702a5ba2d88e5e7094d2b74c283912591d
Shane Jaroch <chown_tee@proton.me>
2026-06-25 10:00:24 -0400

    lint

--- src/service/server_keys/acquire.rs
+++ src/service/server_keys/acquire.rs
@@ -4,13 +4,11 @@ use std::{
 	time::Duration,
 };
 
-use conduwuit::{
-	debug, debug_error, debug_warn, error, implement, info, result::FlatOk, trace, warn,
-};
+use conduwuit::{debug, debug_error, debug_warn, error, implement, info, trace, warn};
 use futures::{StreamExt, stream::FuturesUnordered};
 use ruma::{
-	CanonicalJsonObject, OwnedServerName, OwnedServerSigningKeyId, ServerName,
-	ServerSigningKeyId, api::federation::discovery::ServerSigningKeys, serde::Raw,
+	OwnedServerName, OwnedServerSigningKeyId, ServerName, ServerSigningKeyId,
+	api::federation::discovery::ServerSigningKeys,
 };
 use serde_json::value::RawValue as RawJsonValue;
 use tokio::time::{Instant, timeout_at};
